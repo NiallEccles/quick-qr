8756032e16aa263556c7241cee7b834f8ecfe5ea
Niall Eccles <nialle@rothco.ie>
2022-10-28 20:39:58 +0100

    Cleanup.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Nav from '../components/Nav'
-import Qr from '../components/Qr'
-import Row from '../components/Row'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <main className=''>
     <Nav/>
-    <Row/>
-    <Row/>
-    <Row/>
-    <Row/>
-    <Row/>
-    <Row/>
     <Component {...pageProps} />
   </main>
 }
