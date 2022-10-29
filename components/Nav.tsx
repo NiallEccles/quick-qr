c825f67dd8c25c58846f0de59409747961e4349b
Niall Eccles <nialle@rothco.ie>
2022-10-30 00:04:09 +0100

    Add links to nav and redirect.

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,8 +1,26 @@
 import type { NextPage } from "next";
+import Link from "next/link";
+import PrimaryButton from "./Buttons/PrimaryButton";
+import SecondaryButton from "./Buttons/SecondaryButton";
 import TertiaryButton from "./Buttons/TertiaryButton";
+import QuaternaryButton from "./Buttons/QuaternaryButton";
+
 import QrIcon from "./Icons/QrIcon";
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/router";
 
 const Nav: NextPage = () => {
+  const session = useSession();
+  const supabase = useSupabaseClient();
+  const router = useRouter();
+
+  const signOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    if(!error){
+      router.push('/');
+    }
+  }
+
   return (
     <div className="navbar bg-base-100 mb-5">
       {/* <div className="flex-none">
@@ -23,12 +41,30 @@ const Nav: NextPage = () => {
         </button>
       </div> */}
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl"><QrIcon/><span className="ml-2">Quick QR</span></a>
+        <Link href="/">
+          <a className="btn btn-ghost normal-case text-xl">
+            <QrIcon />
+            <span className="ml-2">Quick QR</span>
+          </a>
+        </Link>
       </div>
       <div className="flex align-center">
-        <div>
-          <TertiaryButton>Create QR</TertiaryButton>
-        </div>
+        <Link href="/create">
+          <a>
+            <TertiaryButton>Create QR</TertiaryButton>
+          </a>
+        </Link>
+        {!session ? (
+          <Link href="/login">
+            <a>
+              <PrimaryButton>Login</PrimaryButton>
+            </a>
+          </Link>
+        ) : (
+          <a onClick={()=>signOut()}>
+            <QuaternaryButton>Logout</QuaternaryButton>
+          </a>
+        )}
         <button className="btn btn-square btn-ghost ml-3">
           <svg
             xmlns="http://www.w3.org/2000/svg"
