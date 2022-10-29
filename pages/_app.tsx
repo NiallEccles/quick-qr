b398828f5401d65736dc84edec4950d486bad647
Niall Eccles <nialle@rothco.ie>
2022-10-30 00:06:41 +0100

    Supabase state wrapper component.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,30 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Nav from '../components/Nav'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Nav from "../components/Nav";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { useState } from "react";
+import { NextComponentType, NextPageContext } from "next";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <main className=''>
-    <Nav/>
-    <Component {...pageProps} />
-  </main>
+function MyApp({
+  Component,
+  pageProps,
+}: {
+  Component: NextComponentType<NextPageContext, any, any>;
+  pageProps: { initialSession: any };
+}) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <main className="max-w-6xl mx-auto">
+        <Nav />
+        <Component {...pageProps} />
+      </main>
+    </SessionContextProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
